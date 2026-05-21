2ef457626b810ea266a0dc71202710d5d4d82c91
ali <m4567s019283@gmail.com>
2026-05-21 16:31:47 +0300

    commit

--- admins/app/admin/products/[id]/page.tsx
+++ admins/app/admin/products/[id]/page.tsx
@@ -1,7 +1,7 @@
 import FormModel from "@/components/products/formModel";
 export default async function page({params}:{params:Promise<{id:string}>}) {
     const {id}=await params
-    const data=await fetch(`${process.env.NEXTAUTH_URL}/api/products/${id}`)
+    const data=await fetch(`https://next-shop-xamf.vercel.app/api/products/${id}`)
     .then((res)=>res.json())
     console.log(data)
   return (
